5109a2292c7b70ecda6d4f382679cc8b2ff530d4
Tharshan <tharshan09@gmail.com>
2012-10-11 00:32:55 +0100

    updated readme

--- README.md
+++ README.md
@@ -2,3 +2,103 @@ gsmx
 ====
 
 A quick env to convert a mix on designers.mx to grooveshark using scrapy and Tinysong API
+
+Usage
+====
+`python get_id.py`
+
+Edit `url` in get_id.py variable to get ID's working for a different playlist.
+Edit grooveshark.py to add your KEY and SECRET provided to you by grooveshark
+Edit get_id.py line 14 to add your own username and password for grooveshark so it can add playlists to your own account.
+
+TODO
+====
+* To use the grooveshark api to create playlists for any account when logged in using OAuth
+* To generalise the script a bit more so that it can be done with any spotify playlist on the web, not just designers.mx
+
+Sample Output
+====
+from `cat playlist_futureMe_f-a-l-l_1728.json | python -mjson.tool`
+```
+{
+    "AlbumID": 7161817,
+    "AlbumName": "Kill for Love",
+    "ArtistID": 20984,
+    "ArtistName": "Chromatics",
+    "SongID": 33463149,
+    "SongName": "Kill For Love",
+    "Url": "http://tinysong.com/MW2S"
+}{
+    "AlbumID": 1264393,
+    "AlbumName": "Pet Grief",
+    "ArtistID": 404316,
+    "ArtistName": "The Radio Dept.",
+    "SongID": 35362351,
+    "SongName": "I Wanted You to Feel the Same",
+    "Url": "http://tinysong.com/10VYn"
+}{
+    "AlbumID": 4380400,
+    "AlbumName": "In Evening Air",
+    "ArtistID": 698851,
+    "ArtistName": "Future Islands",
+    "SongID": 25822722,
+    "SongName": "Inch Of Dust",
+    "Url": "http://tinysong.com/HF8P"
+}{
+    "AlbumID": 6141745,
+    "AlbumName": "Within and Without",
+    "ArtistID": 1187703,
+    "ArtistName": "Washed Out",
+    "SongID": 31337051,
+    "SongName": "Soft",
+    "Url": "http://tinysong.com/HOgN"
+}{
+    "AlbumID": 6623112,
+    "AlbumName": "Last Night (Original Motion Picture Soundtrack)",
+    "ArtistID": 404087,
+    "ArtistName": "Bat for Lashes",
+    "SongID": 31786679,
+    "SongName": "Daniel",
+    "Url": "http://tinysong.com/Of6h"
+}{
+    "AlbumID": 7809227,
+    "AlbumName": "Mixed Emotions///eastvillageclub.tumblr.com",
+    "ArtistID": 1159528,
+    "ArtistName": "Tanlines",
+    "SongID": 35530078,
+    "SongName": "Abby",
+    "Url": "http://tinysong.com/13SLK"
+}{
+    "AlbumID": 3359329,
+    "AlbumName": "Movement (Collector's Edition)",
+    "ArtistID": 595,
+    "ArtistName": "New Order",
+    "SongID": 25940369,
+    "SongName": "Ceremony [Alternate 12\" Version]",
+    "Url": "http://tinysong.com/vT9L"
+}{
+    "AlbumID": 7573719,
+    "AlbumName": "Passage",
+    "ArtistID": 1667162,
+    "ArtistName": "Exitmusic",
+    "SongID": 36236530,
+    "SongName": "The Sea",
+    "Url": "http://tinysong.com/17A1g"
+}{
+    "AlbumID": 2784856,
+    "AlbumName": "The Warning",
+    "ArtistID": 3821,
+    "ArtistName": "Hot Chip",
+    "SongID": 9015011,
+    "SongName": "So Glad to See You",
+    "Url": "http://tinysong.com/iD7O"
+}{
+    "AlbumID": 7323507,
+    "AlbumName": "We All Shine",
+    "ArtistID": 1462940,
+    "ArtistName": "Team Ghost",
+    "SongID": 33940368,
+    "SongName": "A Glorious Time",
+    "Url": "http://tinysong.com/14BBv"
+}
+```
--- get_id.py
+++ get_id.py
@@ -7,33 +7,14 @@ from scrapy.http import Request, Response
 import subprocess
 import simplejson
 import os.path
+import grooveshark
+from secrets import *
 def main():
-	"""
-	#########
-	Sample output
-	#########
-	Kill+For+Love+Chromatics
-	{u'SongID': 33463149, u'Url': u'http://tinysong.com/MW2S', u'ArtistName': u'Chromatics', u'AlbumName': u'Kill for Love', u'AlbumID': 7161817, u'ArtistID': 20984, u'SongName': u'Kill For Love'}
-	I+Wanted+You+To+Feel+The+Same+The+Radio+Dept.
-	{u'SongID': 35362351, u'Url': u'http://tinysong.com/10VYn', u'ArtistName': u'The Radio Dept.', u'AlbumName': u'Pet Grief', u'AlbumID': 1264393, u'ArtistID': 404316, u'SongName': u'I Wanted You to Feel the Same'}
-	Inch+of+Dust+Future+Islands
-	{u'SongID': 25822722, u'Url': u'http://tinysong.com/HF8P', u'ArtistName': u'Future Islands', u'AlbumName': u'In Evening Air', u'AlbumID': 4380400, u'ArtistID': 698851, u'SongName': u'Inch Of Dust'}
-	Soft+Washed+Out
-	{u'SongID': 31337051, u'Url': u'http://tinysong.com/HOgN', u'ArtistName': u'Washed Out', u'AlbumName': u'Within and Without', u'AlbumID': 6141745, u'ArtistID': 1187703, u'SongName': u'Soft'}
-	Daniel+Bat+For+Lashes
-	{u'SongID': 31786679, u'Url': u'http://tinysong.com/Of6h', u'ArtistName': u'Bat for Lashes', u'AlbumName': u'Last Night (Original Motion Picture Soundtrack)', u'AlbumID': 6623112, u'ArtistID': 404087, u'SongName': u'Daniel'}
-	Abby+Tanlines
-	{u'SongID': 35530078, u'Url': u'http://tinysong.com/13SLK', u'ArtistName': u'Tanlines', u'AlbumName': u'Mixed Emotions///eastvillageclub.tumblr.com', u'AlbumID': 7809227, u'ArtistID': 1159528, u'SongName': u'Abby'}
-	Ceremony+[Alternate+12"+Version]+New+Order
-	{u'SongID': 25940369, u'Url': u'http://tinysong.com/vT9L', u'ArtistName': u'New Order', u'AlbumName': u"Movement (Collector's Edition)", u'AlbumID': 3359329, u'ArtistID': 595, u'SongName': u'Ceremony [Alternate 12" Version]'}
-	Passage+Exitmusic
-	{u'SongID': 36236530, u'Url': u'http://tinysong.com/17A1g', u'ArtistName': u'Exitmusic', u'AlbumName': u'Passage', u'AlbumID': 7573719, u'ArtistID': 1667162, u'SongName': u'The Sea'}
-	So+Glad+To+See+You+Hot+Chip
-	{u'SongID': 9015011, u'Url': u'http://tinysong.com/iD7O', u'ArtistName': u'Hot Chip', u'AlbumName': u'The Warning', u'AlbumID': 2784856, u'ArtistID': 3821, u'SongName': u'So Glad to See You'}
-	+A+Glorious+Time+Team+Ghost
-	{u'SongID': 33940368, u'Url': u'http://tinysong.com/14BBv', u'ArtistName': u'Team Ghost', u'AlbumName': u'We All Shine', u'AlbumID': 7323507, u'ArtistID': 1462940, u'SongName': u'A Glorious Time'}
-	"""
-	url = "http://designers.mx/post/detail/futureMe/f-a-l-l/1728/"
+	grooveshark.init()
+	grooveshark.authenticate_user(YOUR_USERNAME, YOUR_PASSWORD)
+	songIDs = []
+	songdata = []
+	url = "http://designers.mx/post/detail/Clemensposch/hope-love/1845/"
 	user = url.split("/")[-4]
 	mix = url.split("/")[-3]
 	mix_id = url.split("/")[-2]
@@ -50,15 +31,26 @@ def main():
 		for item in data:
 			query = item['query']
 			r = requests.get('http://tinysong.com/b/%s?format=json&key=0fc107bfb325fdb787d837b406d80d04' % (query))
-			playlist = open('playlist_'+filename, 'a')
-			load = simplejson.loads(r.text)
-			playlist.write(simplejson.dumps(load, indent=4, sort_keys=True))
+			if r.json != []:
+				print "Found song: %s " % r.json['SongName']
+				songIDs.append(r.json['SongID'])
+				songdata.append(r.json)
+		playlist = open('playlist_'+filename, 'w')
+		playlist.write(simplejson.dumps(songdata, indent=4))
+		playlist.close()
 	json_data.close()
 	if os.path.isfile('playlist_'+filename) == False:
-		playlist.close()
+		pass
 	else:
-		print(open('playlist_'+filename, 'r').read())
-
-
+		data = open('playlist_'+filename, 'r').read()
+		# print data
+		songdata = simplejson.loads(data)
+		for item in songdata:
+			songIDs.append(item['SongID'])
+	print songIDs
+	user_input = raw_input("Do you want to create a playlist on grooveshark? y/n ")
+	if user_input == 'y':
+		resp = grooveshark.api_call('createPlaylist', {'name':'Designs.MX %s %s' % (user, mix),'songIDs':songIDs})
+		print resp
 if __name__ == '__main__':
 	main()
